16ac9cbae0e715958b98149f589c67e425983d47
Rafael J Garcia <ekoe299@gmail.com>
2021-05-21 15:25:34 -0700

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h3>Blackjack Security continually strives to deliver professional services to customers throughout the greater Los Angeles area</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
