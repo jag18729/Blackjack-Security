621a8ab5d73fa5d2af31bae65939e3f85409a472
Rafael J Garcia <ekoe299@gmail.com>
2021-05-23 16:43:58 -0700

    Added Particles Components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            },
+            shape: {
+              type: "square",
+              stroke: {
+                width: 6,
+                color: "tan",
+              }
+            }
+          }
+        }
+        }
+      />
       <NavBar />
       <Header />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import React from 'react';
 import Typed from 'react-typed';
 
@@ -6,6 +7,18 @@ const Header = () => {
     <div className="header-wrapper">
       <div className="main-info">
         <h3>Blackjack Security continually strives to deliver professional services to customers throughout the greater Los Angeles area</h3>
+        <Typed
+          className="typed-text"
+          strings={["Residential Security", "Personal Detail Security", "Property Security", "We can arrange it to protect you and your business"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+
+        <a href="">
+          <Button variant="outline-danger" class="btn-main-offer" to="">Contact Me
+          </Button>
+        </a>
       </div>
     </div>
   );
