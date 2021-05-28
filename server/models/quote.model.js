640215b3b3b8f363c5d16b4810bb858ae8db5519
Rafael J Garcia <ekoe299@gmail.com>
2021-05-28 14:55:41 -0700

    Creates Quote model and routes

---
+++ server/models/quote.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const QuoteSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: [true, "Please enter your first name."],
+    minlength: [3, "First name must be at least 3 characters."]
+  },
+  last_name: {
+    type: String,
+    required: [true, "Please enter your last name."],
+    minlength: [3, "Last name must be at least 3 characters."]
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email address."],
+  },
+  company: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: [true, "Please enter a valid city"],
+    minlength: [3, "City must be at least 3 characters."]
+  },
+  phone: {
+    type: Number,
+    minlength: [3, "Phone number must be at least 10 digits."],
+    required: [true, "Please enter the best phone number to reach out you."],
+  },
+  location: {
+    type: String,
+    required: [true, "Please enter a location."],
+  },
+  service: {
+    type: String,
+    required: [true, "Please select a service."],
+  },
+  description: {
+    type: String,
+  },
+}, { timestamps: true });
+
+
+module.exports.Quote = mongoose.model("Quote", QuoteSchema);
