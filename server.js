50f9e140faf3073818fc67dec5fe0cacf5d65585
Rafael J Garcia <ekoe299@gmail.com>
2021-05-29 11:59:25 -0700

    Cleated Mongo DB and linked it to the backend. Tested CRUD routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
+require('./server/config/mongoose.config');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+require('./server/routes/quote.routes')(app);
 app.listen(8000, () => {
-  console.log("Your are now listening to port 8000")
-})
+  console.log('listening on port 8000')
+}) 
--- server/routes/quote.routes.js
+++ server/routes/quote.routes.js
@@ -1,8 +1,8 @@
 const QuoteController = require('../controllers/controller.quote');
 
 module.exports = function (app) {
-  app.post('/api/quote/new', QuoteController.createQuote);
   app.get('/api/quotes', QuoteController.getAllQuotes);
+  app.post('/api/quote', QuoteController.createQuote);
   app.get('/api/quote/:id', QuoteController.getQuote);
   app.put('/api/quote/:id', QuoteController.updateQuote);
   app.delete('/api/quote/:id', QuoteController.deleteQuote);
